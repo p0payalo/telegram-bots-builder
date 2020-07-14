4c0f79ab7e5075f8b9cebd30fbd8e0be83ecb170
Vyacheslav <slavikeptask@gmail.com>
2020-07-14 07:24:09 +0300

    fix try catch block

--- Controllers/BotsController.cs
+++ Controllers/BotsController.cs
@@ -27,124 +27,126 @@ namespace WebTelegramBotsBuilder.Controllers
         [Route("/edit")]
         public async Task<IActionResult> EditBot(int Id)
         {
-            ViewData["name"] = User.Identity.Name;
-            Models.User user = await db.Users.Include(x => x.Bots).ThenInclude(x => x.BotQueries)
-                .ThenInclude(x => x.Response).FirstOrDefaultAsync(x => x.Name == User.Identity.Name);
-            TelegramBot model;
             try
             {
+                ViewData["name"] = User.Identity.Name;
+                Models.User user = await db.Users.Include(x => x.Bots).ThenInclude(x => x.BotQueries)
+                    .ThenInclude(x => x.Response).FirstOrDefaultAsync(x => x.Name == User.Identity.Name);
+                TelegramBot model;
                 model = user.Bots.FirstOrDefault(x => x.Id == Id);
+                return View("EditBot", model);
             }
             catch
             {
                 return View("Error", new ErrorModel("Bad bot index"));
             }
-            return View("EditBot", model);
         }
 
         [HttpPost]
         [Route("/editbot")]
         public async Task<IActionResult> UpdateBot(int Id, string BotName, string BotToken)
         {
-            Models.User user = await db.Users.Include(x => x.Bots).FirstOrDefaultAsync(x => x.Name == User.Identity.Name);
-            TelegramBot bot;
             try
             {
+                Models.User user = await db.Users.Include(x => x.Bots).FirstOrDefaultAsync(x => x.Name == User.Identity.Name);
+                TelegramBot bot;
                 bot = user.Bots.First(x => x.Id == Id);
                 bot.BotName = BotName;
                 bot.BotToken = BotToken;
                 db.Bots.Update(bot);
                 await db.SaveChangesAsync();
+                return RedirectToAction("EditBot", new { Id });
             }
             catch
             {
                 return View("Error", new ErrorModel("Bad bot index"));
             }
-            return RedirectToAction("EditBot", new { Id });
         }
 
         [HttpPost]
         [Route("/addquery")]
         public async Task<IActionResult> AddQuery(int Id, string Query, string Response)
         {
-            Models.User user = await db.Users.Include(x => x.Bots).ThenInclude(x => x.BotQueries)
-                .ThenInclude(x => x.Response).FirstOrDefaultAsync(x => x.Name == User.Identity.Name);
-            TelegramBot bot;
             try
             {
+                Models.User user = await db.Users.Include(x => x.Bots).ThenInclude(x => x.BotQueries)
+                .ThenInclude(x => x.Response).FirstOrDefaultAsync(x => x.Name == User.Identity.Name);
+                TelegramBot bot;
                 bot = user.Bots.First(x => x.Id == Id);
                 bot.BotQueries.Add(new BotQuery(Query, new BotResponse(Response, MessageType.Text), MessageType.Text));
                 db.Update(bot);
                 await db.SaveChangesAsync();
+                return RedirectToAction("EditBot", new { Id });
             }
             catch
             {
                 return View("Error", new ErrorModel("Bad bot index"));
             }
-            return RedirectToAction("EditBot", new { Id });
         }
 
         [HttpPost]
         [Route("/removequery")]
         public async Task<IActionResult> RemoveQuery(int Id, int BotId)
         {
-            Models.User user = await db.Users.Include(x => x.Bots).ThenInclude(x => x.BotQueries)
-                .ThenInclude(x => x.Response).FirstOrDefaultAsync(x => x.Name == User.Identity.Name);
-            TelegramBot bot = user.Bots.FirstOrDefault(x => x.Id == BotId);
             try
             {
+                Models.User user = await db.Users.Include(x => x.Bots).ThenInclude(x => x.BotQueries)
+                .ThenInclude(x => x.Response).FirstOrDefaultAsync(x => x.Name == User.Identity.Name);
+                TelegramBot bot = user.Bots.FirstOrDefault(x => x.Id == BotId);
                 db.BotQueries.Remove(bot.BotQueries.First(x => x.Id == Id));
+                await db.SaveChangesAsync();
+                return RedirectToAction("EditBot", new { Id = BotId });
             }
             catch
             {
                 return View("Error", new ErrorModel("Bad query id"));
             }
-            await db.SaveChangesAsync();
-            return RedirectToAction("EditBot", new { Id = BotId });
         }
 
         [HttpPost]
         [Route("/updatequery")]
         public async Task<IActionResult> UpdateQuery(int Id, int BotId, string Query, string Response)
         {
-            Models.User user = await db.Users.Include(x => x.Bots).ThenInclude(x => x.BotQueries)
-                .ThenInclude(x => x.Response).FirstOrDefaultAsync(x => x.Name == User.Identity.Name);
-            BotQuery target = null;
             try
             {
+                Models.User user = await db.Users.Include(x => x.Bots).ThenInclude(x => x.BotQueries)
+                .ThenInclude(x => x.Response).FirstOrDefaultAsync(x => x.Name == User.Identity.Name);
+                BotQuery target = null;
+
                 TelegramBot bot = user.Bots.FirstOrDefault(x => x.Id == BotId);
                 target = bot.BotQueries.First(x => x.Id == Id);
                 target.Value = Query;
                 target.Response.Value = Response;
+                db.BotQueries.Update(target);
+                await db.SaveChangesAsync();
+                return RedirectToAction("EditBot", new { Id = BotId });
             }
             catch
             {
                 return View("Error", new ErrorModel("Bad query id"));
             }
-            db.BotQueries.Update(target);
-            await db.SaveChangesAsync();
-            return RedirectToAction("EditBot", new { Id = BotId });
         }
 
         [HttpGet]
         [Route("/updatequery")]
         public async Task<IActionResult> UpdateQuery(int Id, int BotId)
         {
-            ViewData["name"] = User.Identity.Name;
-            Models.User user = await db.Users.Include(x => x.Bots).ThenInclude(x => x.BotQueries)
-                .ThenInclude(x => x.Response).FirstOrDefaultAsync(x => x.Name == User.Identity.Name);
-            BotQuery target;
             try
             {
+                ViewData["name"] = User.Identity.Name;
+                Models.User user = await db.Users.Include(x => x.Bots).ThenInclude(x => x.BotQueries)
+                    .ThenInclude(x => x.Response).FirstOrDefaultAsync(x => x.Name == User.Identity.Name);
+                BotQuery target;
+
                 TelegramBot bot = user.Bots.FirstOrDefault(x => x.Id == BotId);
                 target = bot.BotQueries.First(x => x.Id == Id);
+                ViewBag.BotId = BotId;
+                return View("EditQuery", target);
             }
             catch
             {
                 return View("Error", new ErrorModel("Bad query id"));
             }
-            ViewBag.BotId = BotId;
-            return View("EditQuery", target);
         }
     }
 }

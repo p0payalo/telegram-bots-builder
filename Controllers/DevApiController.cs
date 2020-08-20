e700102bb34db43ea9d7498d89607625329cceeb
Vyacheslav <slavikeptask@gmail.com>
2020-08-20 20:54:22 +0300

    crud for queries in api

--- Controllers/DevApiController.cs
+++ Controllers/DevApiController.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿#pragma warning disable CS8632
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using BotCreator.Core;
+using BotCreator.Core.BotQueries;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -34,6 +36,18 @@ namespace WebTelegramBotsBuilder.Controllers
             }
         }
 
+        [NonAction]
+        private ApiRequestResponse Error(string message)
+        {
+            return new ApiRequestResponse() { Status = false, Message = message };
+        }
+
+        [NonAction]
+        private ApiRequestResponse Success(string message)
+        {
+            return new ApiRequestResponse() { Status = true, Message = message };
+        }
+
         [HttpPost]
         [Route("[action]")]
         public async Task<ActionResult<ApiResponse>> Auth(string Username, string Password)
@@ -48,18 +62,18 @@ namespace WebTelegramBotsBuilder.Controllers
                     ApiToken = user.ApiToken
                 };
             }
-            else return new ApiRequestResponse() { Status = false, Message = "Wrong username or password" };
-            
+            else return Error("Wrong username or password");
+
         }
 
         //bots methods
-        
+
         [HttpGet]
         [Route("[action]")]
         public async Task<ActionResult<ApiResponse>> GetBots(string Token)
         {
-            Models.User user = await db.Users.Include(b => b.Bots).ThenInclude(q => q.BotQueries).ThenInclude(r => r.Response).FirstOrDefaultAsync(u => u.ApiToken == Token);
-            if (user != null) 
+            Models.User user = await db.Users.Include(b => b.Bots).FirstOrDefaultAsync(u => u.ApiToken == Token);
+            if (user != null)
             {
                 return new ApiBotsResponse()
                 {
@@ -67,36 +81,34 @@ namespace WebTelegramBotsBuilder.Controllers
                     Bots = user.Bots
                 };
             }
-            else return new ApiRequestResponse() { Status = false, Message = "Wrong token" };
+            else return Error("Wrong token");
         }
 
         [HttpPost]
         [Route("[action]")]
-#pragma warning disable CS8632
         public async Task<ActionResult<ApiResponse>> UpdateBot(string Token, int BotId, string? NewName, string? NewToken)
-#pragma warning restore CS8632
         {
             Models.User user = await db.Users.Include(b => b.Bots).FirstOrDefaultAsync(u => u.ApiToken == Token);
             if (user != null)
             {
                 TelegramBot bot = user.Bots.FirstOrDefault(b => b.Id == BotId);
-                if (bot != null) 
+                if (bot != null)
                 {
-                    if(NewName != null)
+                    if (NewName != null)
                     {
                         bot.BotName = NewName;
                     }
-                    if(NewToken != null)
+                    if (NewToken != null)
                     {
                         bot.BotToken = NewToken;
                     }
                     db.Bots.Update(bot);
                     await db.SaveChangesAsync();
-                    return new ApiRequestResponse() { Status = true, Message = "Changes apply" };
+                    return Success("Changes apply");
                 }
-                else return new ApiRequestResponse() { Status = false, Message = "Wrong bot id" };
+                else return Error("Wrong bot id");
             }
-            else return new ApiRequestResponse() { Status = false, Message = "Wrong token" };
+            else return Error("Wrong token");
         }
 
         [HttpDelete]
@@ -104,49 +116,145 @@ namespace WebTelegramBotsBuilder.Controllers
         public async Task<ActionResult<ApiResponse>> DeleteBot(string Token, int BotId)
         {
             Models.User user = await db.Users.Include(b => b.Bots).FirstOrDefaultAsync(u => u.ApiToken == Token);
-            if(user != null)
+            if (user != null)
             {
                 TelegramBot bot = user.Bots.FirstOrDefault(b => b.Id == BotId);
-                if(bot != null)
+                if (bot != null)
                 {
                     db.Bots.Remove(bot);
                     await db.SaveChangesAsync();
-                    return new ApiRequestResponse() { Status = true, Message = "Bot was deleted" };
+                    return Success("Bot was deleted");
                 }
-                else return new ApiRequestResponse() { Status = false, Message = "Wrong bot id" };
+                else return Error("Wrong bot id");
             }
-            else return new ApiRequestResponse() { Status = false, Message = "Wrong token" };
+            else return Error("Wrong token");
         }
 
         [HttpPut]
         [Route("[action]")]
         public async Task<ActionResult<ApiResponse>> CreateBot(string Token, string BotName, string BotToken)
         {
-            if(BotName == null || BotToken == null)
+            if (BotName == null || BotToken == null)
             {
-                return new ApiRequestResponse() { Status = false, Message = "BotName and BotToken is required" };
+                return Error("BotName and BotToken is required");
+            }
+            if (BotName.Length > 12 || BotToken.Length > 32)
+            {
+                return Error("Bot name or bot token is too long");
             }
             Models.User user = await db.Users.Include(b => b.Bots).FirstOrDefaultAsync(u => u.ApiToken == Token);
             if (user != null)
             {
-                if (BotName.Length > 12 || BotToken.Length > 32)
+                TelegramBot bot = new TelegramBot(BotToken, BotName);
+                (user.Bots as List<TelegramBot>).Add(bot);
+                db.Users.Update(user);
+                await db.SaveChangesAsync();
+                return new ApiBotsResponse()
                 {
-                    return new ApiRequestResponse() { Status = false, Message = "Bot name or bot token too long" };
+                    Status = true,
+                    Bots = user.Bots
+                };
+            }
+            else return Error("Wrong token");
+        }
+
+        //queries methods
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<ActionResult<ApiResponse>> GetQueries(string Token, int BotId)
+        {
+            Models.User user = await db.Users.Include(b => b.Bots).FirstOrDefaultAsync(u => u.ApiToken == Token);
+            if (user != null)
+            {
+                TelegramBot bot = user.Bots.FirstOrDefault(b => b.Id == BotId);
+                if (bot != null)
+                {
+                    return new ApiQueriesResponse() { Status = true, Queries = bot.BotQueries };
                 }
-                else
+                else return Error("Wrong bot id");
+            }
+            else return Error("Wrong token");
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<ActionResult<ApiResponse>> UpdateQuery(string Token, int BotId, int QueryId, string? Query, string? Response)
+        {
+            Models.User user = await db.Users.Include(b => b.Bots).FirstOrDefaultAsync(u => u.ApiToken == Token);
+            if (user != null)
+            {
+                TelegramBot bot = user.Bots.FirstOrDefault(b => b.Id == BotId);
+                if (bot != null)
                 {
-                    TelegramBot bot = new TelegramBot(BotToken, BotName);
-                    (user.Bots as List<TelegramBot>).Add(bot);
-                    db.Users.Update(user);
+                    BotCreator.Core.BotQueries.BotQuery query = bot.BotQueries.FirstOrDefault(q => q.Id == QueryId);
+                    if (query != null)
+                    {
+                        if (Query != null)
+                        {
+                            query.Value = Query;
+                        }
+                        if (Response != null)
+                        {
+                            query.Response.Value = Response;
+                        }
+                        db.BotQueries.Update(query);
+                        await db.SaveChangesAsync();
+                        return Success("Changes apply.");
+                    }
+                    else return Error("Wrong quuery id");
+                }
+                else return Error("Wrong bot id");
+            }
+            else return Error("Wrong token");
+        }
+
+        [HttpDelete]
+        [Route("[action]")]
+        public async Task<ActionResult<ApiResponse>> DeleteQuery(string Token, int BotId, int QueryId)
+        {
+            Models.User user = await db.Users.Include(b => b.Bots).FirstOrDefaultAsync(u => u.ApiToken == Token);
+            if (user != null)
+            {
+                TelegramBot bot = user.Bots.FirstOrDefault(b => b.Id == BotId);
+                if (bot != null)
+                {
+                    BotCreator.Core.BotQueries.BotQuery query = bot.BotQueries.FirstOrDefault(q => q.Id == QueryId);
+                    if (query != null)
+                    {
+                        db.BotQueries.Remove(query);
+                        await db.SaveChangesAsync();
+                        return Success("Query was deleted");
+                    }
+                    else return Error("Wrong query id");
+                }
+                else return Error("Wrong bot id");
+            }
+            else return Error("Wrong token");
+        }
+
+        [HttpPut]
+        [Route("[action]")]
+        public async Task<ActionResult<ApiResponse>> CreateQuery(string Token, int BotId, string Query, string Response)
+        {
+            Models.User user = await db.Users.Include(b => b.Bots).FirstOrDefaultAsync(u => u.ApiToken == Token);
+            if (user != null)
+            {
+                TelegramBot bot = user.Bots.FirstOrDefault(b => b.Id == BotId);
+                if (bot != null)
+                {
+                    bot.BotQueries.Add(new BotQuery(Query, new BotResponse(Response, MessageType.Text), MessageType.Text));
+                    db.Bots.Update(bot);
                     await db.SaveChangesAsync();
-                    return new ApiBotsResponse()
+                    return new ApiQueriesResponse()
                     {
                         Status = true,
-                        Bots = user.Bots
+                        Queries = bot.BotQueries
                     };
                 }
+                else return Error("Wrong bot id");
             }
-            else return new ApiRequestResponse() { Status = false, Message = "Wrong token" };
+            else return Error("Wrong token");
         }
     }
 }
---
+++ Models/ApiResponses/ApiQueriesResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebTelegramBotsBuilder.Models.ApiResponses
+{
+    public class ApiQueriesResponse : ApiResponse
+    {
+        public IEnumerable<BotCreator.Core.BotQueries.BotQuery> Queries { get; set; }
+    }
+}
